ce93e496bbeb47833fcab78e69d04da22900ea0d
mzulfucelik <muhammedzulfucelik@gmail.com>
2023-01-09 00:03:48 +0300

    Add project files.

---
+++ SupperCRMExample.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SupperCRMExample.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
